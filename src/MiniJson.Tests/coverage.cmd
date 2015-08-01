c595790ff3727a788b7ba4be8ecb0dbe2f285197
mrange <marten_range@hotmail.com>
2015-08-01 10:09:21 +0200

    Added OpenCover

---
+++ src/MiniJson.Tests/coverage.cmd
@@ -0,0 +1,3 @@
+..\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -target:"bin\Debug\MiniJson.Tests.exe" -filter:"+[MiniJson]*"
+..\packages\ReportGenerator.2.1.8.0\tools\ReportGenerator.exe -reports:bin\Debug\results.xml -targetdir:bin\Debug\coverage
+.\bin\Debug\coverage\index.htm
