92c7cfb77e06b371956d9edb5ae67760928f835f
mrange <marten_range@hotmail.com>
2015-08-03 13:12:32 +0200

    Published NuGet/2.3.0.0

--- src/Global_AssemblyInfo.fs
+++ src/Global_AssemblyInfo.fs
@@ -29,7 +29,7 @@ open System.Runtime.InteropServices
 
 [<assembly: ComVisible(false)>]
 
-[<assembly: AssemblyVersion("2.2.5.0")>]
-[<assembly: AssemblyFileVersion("2.2.5.0")>]
+[<assembly: AssemblyVersion("2.3.0.0")>]
+[<assembly: AssemblyFileVersion("2.3.0.0")>]
 do
     ()
